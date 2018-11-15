a3ae76df78814af8f72a3dd0e067cf0b977910a7
uday768 <uday768@gmail.com>
2018-11-15 17:20:39 +0530

    Organization Service

--- organization-service/src/main/java/com/thoughtmechanix/organization/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/OrganizationServiceApplication.java
@@ -1,7 +1,9 @@
 package com.thoughtmechanix.organization;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -10,6 +12,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableEurekaClient
 @EnableHystrix
 @EnableCircuitBreaker
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
---
+++ organization-service_1/src/main/java/com/thoughtmechanix/organization/OrganizationService1Application.java
@@ -0,0 +1,18 @@
+package com.thoughtmechanix.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+public class OrganizationService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationService1Application.class, args);
+	}
+}
